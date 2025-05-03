ab89e89783aa0a3f4b05f3d0a3c5b3a3442ba668
Shevchuk Vladyslav <ipz231_shvye@student.ztu.edu.ua>
2025-05-04 00:39:47 +0300

    Iterator

---
+++ Lab-3/Composite/CompositePattern/Iterator/ILightIterator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Composite.CompositePattern.Iterator
+{
+    public interface ILightIterator
+    {
+        bool HasNext();
+        LightNode Next();
+        void Reset();
+    }
+}
---
+++ Lab-3/Composite/CompositePattern/Iterator/LightBreadthFirstIterator.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Composite.CompositePattern.Iterator
+{
+    public class LightBreadthFirstIterator : ILightIterator
+    {
+        private LightElementNode _root;
+        private Queue<LightNode> _queue;
+        private LightNode _current;
+        private bool _includeRoot;
+
+        public LightBreadthFirstIterator(LightElementNode root, bool includeRoot = true)
+        {
+            _root = root;
+            _includeRoot = includeRoot;
+            Reset();
+        }
+
+        public bool HasNext()
+        {
+            return _queue.Count > 0;
+        }
+
+        public LightNode Next()
+        {
+            if (!HasNext())
+            {
+                throw new InvalidOperationException("No more elements to iterate");
+            }
+
+            _current = _queue.Dequeue();
+
+            if (_current is LightElementNode elementNode)
+            {
+                foreach (var child in elementNode._children)
+                {
+                    _queue.Enqueue(child);
+                }
+            }
+
+            return _current;
+        }
+
+        public void Reset()
+        {
+            _queue = new Queue<LightNode>();
+            _current = null;
+
+            if (_includeRoot)
+            {
+                _queue.Enqueue(_root);
+            }
+            else
+            {
+                foreach (var child in _root._children)
+                {
+                    _queue.Enqueue(child);
+                }
+            }
+        }
+    }
+}
